c2f23a847c8abc4619a399d34dd1ab4f89251b81
Tanusha <tanushachoudhary18@gmail.com>
2025-03-02 14:58:44 +0530

    changed api end point to deployed url

--- frontend/src/components/utils/constant.js
+++ frontend/src/components/utils/constant.js
@@ -1,5 +1,8 @@
-export const USER_API_END_POINT = "http://localhost:5000/api/v1/user";
-export const JOB_API_END_POINT = "http://localhost:5000/api/v1/job";
+export const USER_API_END_POINT =
+  "https://job-portal-mb94.onrender.com/api/v1/user";
+export const JOB_API_END_POINT =
+  "https://job-portal-mb94.onrender.com/api/v1/job";
 export const APPLICATION_API_END_POINT =
-  "http://localhost:5000/api/v1/application";
-export const COMPANY_API_END_POINT = "http://localhost:5000/api/v1/company";
+  "https://job-portal-mb94.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT =
+  "https://job-portal-mb94.onrender.com/api/v1/company";
