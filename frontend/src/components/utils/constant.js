424e5e13742ee4986932ab5e88fe925f43e767c1
Tanusha <tanushachoudhary18@gmail.com>
2025-03-12 22:38:31 +0530

    `Update API end points to production URLs`

--- frontend/src/components/utils/constant.js
+++ frontend/src/components/utils/constant.js
@@ -1,10 +1,10 @@
 export const USER_API_END_POINT =
-  "http://localhost:5173/api/v1/user";
+  "https://job-portal-mb94.onrender.com/api/v1/user";
 export const JOB_API_END_POINT =
-  "http://localhost:5173/api/v1/job";
+  "https://job-portal-mb94.onrender.com/api/v1/job";
 export const APPLICATION_API_END_POINT =
-  "http://localhost:5173/api/v1/application";
+  "https://job-portal-mb94.onrender.com/api/v1/application";
 export const COMPANY_API_END_POINT =
-  "http://localhost:5173/api/v1/company";
+  "https://job-portal-mb94.onrender.com/api/v1/company";
 
 // https://job-portal-mb94.onrender.com
